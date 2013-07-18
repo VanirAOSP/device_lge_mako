0c1c1007c62662bff38fb3f84a42408665a32572
nuclearmistake <nuclearmistake@gmail.com>
2013-07-18 08:36:29 -0400

    Don't explicitly set global cflags in mako's boardconfig
    Change-Id: I97f0e3ac575679bdd8305f08a2a8eaba3de761ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mcpu=cortex-a9 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mcpu=cortex-a9 -mfloat-abi=softfp
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
@@ -24,6 +22,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon-vfpv4
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
